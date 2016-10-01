9120576327fc5c53eda0b4bf9c7bbb1ba4c605c4
Ami <agianchandani2018@pingry.org>
2016-09-30 21:50:53 -0400

    Java Doc format

--- Loops.java
+++ Loops.java
@@ -1,20 +1,42 @@
+/**
+ * This is a project to test different types of Loops
+ * @author Ami Gianchandani
+ * 
+ */
+
 public class Loops
 {
 	public static void main(String[] args) // no return value
 	{
 		whileLoop();
-		
 		System.out.println("--------------------");
+		
 		forLoop();
+		System.out.println("--------------------");
 		
 		nestedWhileLoop(4);
+		System.out.println("--------------------");
 		
 		nestedForLoop(7);
+		System.out.println("--------------------");
 		
-		topTriangle(4);
+		topTriangle(5);
+		System.out.println("--------------------");
 		
 		bottomTriangle(4);
+		System.out.println("--------------------");
+		
+		printStarsTop(5);
+		System.out.println("--------------------");
+		
+		printStarsBottom(4);
 	}
+	
+	/**
+	* <This method prints out a list from 1 - 10 with each number on a new line>
+	* It does this by incrementing a counter up each time through the while loop until it reaches 11 and can no longer enter the loop
+	*/
+	
 	
 	public static void whileLoop()
 	{
@@ -26,6 +48,12 @@ public class Loops
 			
 		}
 	}
+	
+	/**
+	* <This method prints out a list from 0 - 10 with each number on a new line>
+	* It does this by incrementing i, the variable up each time through the for loop until it reaches 11 and can no longer enter the loop
+	*/
+	
 	public static void forLoop()
 	{
 		for(int i = 0; i <= 10; i++)
@@ -33,6 +61,14 @@ public class Loops
 			System.out.println(i);
 		}
 	}
+
+
+	/**
+	* <This method prints out n rows and n columns of stars: n depends on the input>
+	* It does this by printing a star in the first row for every column and then moving to the next row and printing a star for every column there
+	* Uses nested while loops
+	* @param n int that is passed in to the method
+	*/
 
 	public static void nestedWhileLoop(int n)
 	{
@@ -50,6 +86,14 @@ public class Loops
 		}
 	}
 
+
+	/**
+	* <This method prints out n rows and n columns of stars: n depends on the input>
+	* It does this by printing a star in the first row for every column and then moving to the next row and printing a star for every column there
+	* Uses nested for loops
+	* @param n int that is passed in to the method
+	*/
+
 	public static void nestedForLoop (int n)
 	{
 		for(int i = 0; i< n; i++)
@@ -65,6 +109,13 @@ public class Loops
 	}
 	
 	
+	/**
+	* <This method prints out a triangle>
+	* It does this by printing a the same number of stars as the row
+	* Uses nested while loops
+	* @param n int that is passed in to the method
+	*/	
+	
 	public static void topTriangle (int n)
 	{
 		int row = 0;
@@ -74,17 +125,23 @@ public class Loops
 			while (column <= row)
 			{
 				//System.out.println(row);
-				System.out.print("*");
+				System.out.print('*');
 				
 				column ++;
 			}
 			System.out.println();
 			row ++;
 		}
-	
 	}
 	
-	public static  void bottomTriangle (int n)
+	/**
+	* <This method prints out a triangle with the base on top>
+	* It does this by printing the number of stars for the user input number minus the number of rows
+	* Uses nested while loops
+	* @param n int that is passed in to the method
+	*/	
+	
+	public static void bottomTriangle (int n)
 	{
 		int row = 0;
 		while (row < n)
@@ -98,9 +155,48 @@ public class Loops
 			System.out.println();
 			row ++;
 		}
-		
 	}
 	
+	/**
+	* <This method prints out a triangle>
+	* It does this by printing the number of stars for the user input number minus the number of rows
+	* Uses nested for loops
+	* @param n int that is passed in to the method
+	*/	
+		
+	
+	public static void printStarsTop(int n)
+	{
+		for (int row = 0; row < n; row ++)
+		{
+			for (int column = n - row; column >= 1; column --)
+			{
+				System.out.print('*');
+			}
+			System.out.println();
+		}
+	}
+
+	
+	/**
+	* <This method prints out a triangle with the base on top>
+	* It does this by printing the number of stars for the user input number minus the number of rows
+	* Uses nested for loops
+	* @param n int that is passed in to the method
+	*/	
+	
+	public static void printStarsBottom(int n)
+	{
+		for (int row = 0; row < n; row ++)
+		{
+			for (int column = n - row; column > 1; column --)
+			{
+				System.out.print('*');
+			}
+			System.out.println();
+		}
+	}
+
 
 
 
